b6862bba78016565827d212de35e6c761359e249
knight <karvin_jimenez@hotmail.com>
2025-08-24 13:02:58 -0400

    working in the first map.

---
+++ assets/maps/playground/tileset/tileBaseTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tileBaseTileset" tilewidth="16" tileheight="16" tilecount="1350" columns="50">
+ <image source="../img/tileBaseTileset.png" width="800" height="432"/>
+</tileset>
